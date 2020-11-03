e34fb55382a90d44001acf9ecdfd320fcafa3b4d
himanshu soni <er.soni.himanshu@gmail.com>
2020-11-03 21:58:17 +0530

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Added following minimal view
+def post_list(request):
+    return render(request, 'blog/post_list.html', {}) # render (put together) our template blog/post_list.html
